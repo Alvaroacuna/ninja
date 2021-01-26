9c10b9e8ec976087a8f49be81d40b8838e096084
Alvaro Acuña <sebastian20.a.m@gmail.com>
2021-01-26 20:40:35 -0300

    Se añadió Super Ninja

--- index.js
+++ index.js
@@ -11,15 +11,27 @@ class Ninja {
     showStats() {
         console.log(`nombre: ${this.name}\ vida: ${this.vida}\ velocidad: ${this.velocidad}\ fuerza: ${this.fuerza}`);
     }
-    drinSake() {
+    drinkSake() {
         this.vida += 10
         console.log("10 mas de Salud")
     }
 }
 
 class Sensei extends Ninja {
-      
+    constructor(nombre, vida, velocidad = 3, fuerza = 3, sabiduria = 10){
+        super (nombre, vida, velocidad, fuerza)
+        this.sabiduria = sabiduria;
+  }
+  speakWisdom() {
+    console.log('Hola, soy Master Splinter, me tomé un sake de naranja, taba weno');
+    super.drinkSake();
+  }
 }
+
+const ninja_1 = new Ninja('Shen', 10);
+const ninja_2 = new Ninja('Naruto', 13, 4, 5);
+const ninja_3 = new Ninja('Sasuke', 8, 6, 7);
+const superSensei = new Sensei("Master Splinter", 200);
 
 
 
